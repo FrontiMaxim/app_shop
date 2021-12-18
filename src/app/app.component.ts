6de350683b5d2945ab8ba0cdabf03fe8a58abf78
FrontiMaxim <FrontiMax@yandex.ru>
2021-12-18 15:06:19 +0400

    cleaned up the project

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-root',
-    template: `<div>
-                    <app-entry></app-entry>
-               </div>`
-})
-export class AppComponent {}
